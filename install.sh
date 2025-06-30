75469ff7ccaed561f3b3b21056c1e4326e27d082
CjayDoesCode <cjaycodes@protonmail.com>
2025-07-01 03:09:00 +0800

    refactor: add spacing

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@ set -euo pipefail
 # ------------------------------------------------------------------------------
 
 if [[ "$(id -u)" -eq 0 ]]; then
-  echo "This script must not be executed with root privileges."
+  printf "\nThis script must not be executed with root privileges.\n\n"
   exit 1
 fi
 
@@ -57,8 +57,6 @@ theme_pkgs=(
   "papirus-icon-theme"
 )
 
-github_username="CjayDoesCode"
-
 declare -A gsettings_values=(
   ["color-scheme"]="prefer-dark"
   ["cursor-theme"]="capitaine-cursors-light"
@@ -67,6 +65,8 @@ declare -A gsettings_values=(
   ["icon-theme"]="Papirus-Dark"
   ["gtk-theme"]="Orchis-Dark-Compact"
 )
+
+github_username="CjayDoesCode"
 
 # ------------------------------------------------------------------------------
 #   user input
@@ -80,27 +80,32 @@ read -rp $'\n'"Reboot after installation? [Y/n]: " reboot
 #   Installation
 # ------------------------------------------------------------------------------
 
+printf "\nInstalling packages...\n"
 sudo pacman --sync --noconfirm --needed \
   "${pkgs[@]}" \
   "${hyprland_pkgs[@]}" \
   "${font_pkgs[@]}" \
   "${theme_pkgs[@]}"
 
+printf "\nEnabling services...\n"
 systemctl --user enable \
   hyprpaper.service \
   hyprpolkitagent.service \
   waybar.service \
   xdg-user-dirs-update.service
 
+printf "\nConfiguring GTK...\n"
 for key in "${!gsettings_values[@]}"; do
   gsettings set org.gnome.desktop.interface \
     "${key}" \
     "${gsettings_values["${key}"]}"
 done
 
+printf "\nInstalling dotfiles...\n"
 chezmoi init --apply --force "${github_username}"
 
 if [[ ! "${install_osu}" =~ ^[nN] ]]; then
+  printf "\nInstalling osu!(lazer)...\n"
   mkdir --parents ~/Games/osu-lazer
   curl \
     --output ~/Games/osu-lazer/osu.AppImage \
@@ -112,8 +117,10 @@ else
 fi
 
 if [[ "${keep_chezmoi}" =~ ^[nN]$ ]]; then
+  printf "\nRemoving chezmoi...\n"
   chezmoi purge --force
   sudo pacman --remove --nosave --recursive --noconfirm chezmoi
 fi
 
+printf "\nInstallation completed.\n\n"
 [[ ! "${reboot}" =~ ^[nN]$ ]] && reboot

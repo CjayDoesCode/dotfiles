11956ca83101a68e04c37339210f1f41efbbba83
CjayDoesCode <cjaycodes@protonmail.com>
2025-07-24 19:25:41 +0800

    style: change background color and remove border

--- private_dot_config/hypr/scripts/executable_screenshot.sh
+++ private_dot_config/hypr/scripts/executable_screenshot.sh
@@ -2,7 +2,8 @@
 
 timestamp="$(date +%Y%m%d_%H%M%S)"
 output="${HOME}/Pictures/Screenshot_${timestamp}.png"
+region=''
 
-if region="$(slurp)" && grim -g "${region}" "${output}"; then
-  wl-copy <"${output}"
+if region="$(slurp -b '#282828e6' -w 0)"; then
+  grim -g "${region}" "${output}" && wl-copy <"${output}"
 fi
